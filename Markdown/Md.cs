bef5153980e9d8188aebc8f253ab8b7dbca15843
Starcev <starcev_misha@mail.ru>
2017-11-04 17:30:18 +0500

    хех, регурки плохо =(

--- Markdown/Md.cs
+++ Markdown/Md.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 
